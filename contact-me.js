d0edc84e901210f3cf8fccbc4dc7f5e4e59ad1ad
Charles Beall <wacharlb@gmail.com>
2023-11-27 22:00:05 -0800

    Assignment 7 submission completed.

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,59 @@
-// TODO
+const fullname = document.getElementById('name');
+const email = document.getElementById('email');
+const jobTitle = document.getElementById('job-title');
+const companyUrl = document.getElementById('company-url');
+const codingLanguage = document.getElementById('coding-language-kind');
+const form = document.getElementById("contact-me-form");
+let message = document.getElementById('message');
+let reasonSelect = document.getElementById('reason-kind');
+
+const validLength = (input, min) => {
+    if (input.value.trim().length > min) {
+        input.parentElement.classList.remove("invalid");
+        return true
+    } else {
+        input.parentElement.classList.add("invalid");
+        //e.target.title = "Name must be at least 3 characters long";
+        return false;
+    }    
+};
+
+const validateEmail = (email) => {
+    const re = /\w+@\w+\.\w+/;
+    if(re.test(email.value.trim())) {
+        email.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        email.parentElement.classList.add("invalid");
+        return false;
+    }
+}
+
+form.addEventListener("submit", (e) => {    
+    message.value = "";
+    if (!validLength(fullname, 3)) {
+        message.value += "Full name is required and must be at least 3 characters\n";
+    }
+    if (!validateEmail(email)) {
+        message.value += "Email is required and must be valid\n";
+    }
+
+    e.preventDefault();
+});
+
+const handleSelect = (selectElement) => {
+    const selectedValue = selectElement.value;
+    if (selectedValue === "job-opportunity") {
+        // build and append to the DOM an input field for number of employees
+        jobTitle.parentElement.parentElement.classList.remove("hidden");
+        companyUrl.parentElement.parentElement.classList.remove("hidden");
+        codingLanguage.parentElement.classList.add("hidden");
+    } else if (selectedValue == "talk-code") {        
+        // build and append to the DOM an input field for OS
+        jobTitle.parentElement.parentElement.classList.add("hidden");
+        companyUrl.parentElement.parentElement.classList.add("hidden");
+        codingLanguage.parentElement.classList.remove("hidden");
+    }
+}
+
+reasonSelect.addEventListener("change", () => handleSelect(reasonSelect));
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,66 @@
-// TODO
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('lastname');
+const mail = document.getElementById('email');
+const form = document.getElementById('connect-form');
+const os = document.getElementById('operating-system');
+const employees = document.getElementById('num-of-employees');
+
+let select = document.getElementById('contact-kind');
+
+let valid = false;
+
+const validLength = (input, min) => {
+    console.log("validLength");
+    if (input.value.trim().length > min) {
+        input.parentElement.classList.remove("invalid");
+        return true
+    } else {
+        input.parentElement.classList.add("invalid");
+        //e.target.title = "Name must be at least 3 characters long";
+        return false;
+    }
+    console.log(`valid: ${valid}`);
+};
+
+const validateEmail = (email) => {
+    console.log("validateEmail");
+    const re = /\w+@\w+\.\w+/;
+    if(re.test(email.value.trim())) {
+        email.parentElement.classList.remove("invalid");
+        return true;
+    } else {
+        email.parentElement.classList.add("invalid");
+        return false;
+    }
+}
+
+const handleSelect = (selectElement) => {
+    const selectedValue = selectElement.value;
+    if (selectedValue === "business") {
+        // build and append to the DOM an input field for number of employees
+        employees.parentElement.classList.remove("hidden");
+        os.parentElement.classList.add("hidden");
+    } else if (selectedValue == "technical") {        
+        // build and append to the DOM an input field for OS
+        employees.parentElement.classList.add("hidden");
+        os.parentElement.classList.remove("hidden");
+    }
+}
+
+select.addEventListener("change", () => handleSelect(select));
+
+form.addEventListener("submit", (e) => {
+    console.log("addEventListener");
+    // e.preventDevault*()
+    if (validLength(firstName, 3) && validLength(firstName, 3) && validateEmail(email)) {
+        valid = true;        
+    } else {
+        valid = false;
+        //e.target.title = "Name must be at least 3 characters long";
+        e.preventDefault();
+        console.log("Bad input");
+    }
+
+});
+
+console.log(`test`);
